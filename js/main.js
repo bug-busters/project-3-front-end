aeea06dd775c8cc174b5aed5f579e16693142e8f
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-24 10:07:14 -0400

    Add hide() and show() toggle for logout button

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function () {
+
+  // hide logout button until user is logged in
+  $('#logout').hide();
+
+  // toggle Register/Login navbar link with Logout
+  // when user is logged in // $('#logout').show();
+
+
+
+});
+
