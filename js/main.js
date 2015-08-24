2d1eea698cd0f1d82dc59db6ef6a8984dfaafd78
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-24 10:54:26 -0400

    Add handlers for login and register

--- js/main.js
+++ js/main.js
@@ -2,12 +2,62 @@
 
 $(document).ready(function () {
 
+  // url root
+  var sa = 'http://localhost:3000';
+
   // hide logout button until user is logged in
   $('#logout').hide();
 
-  // toggle Register/Login navbar link with Logout
-  // when user is logged in // $('#logout').show();
+  // FIX ME -- need to match credentials keys to backend db
+  // user register
+  $('#register').on('click', function() {
+    $.ajax(sa + '/signup', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          firstname: $('#first-name').val(),
+          lastname: $('#last-name').val(),
+          email: $('#reg_email').val(),
+          password: $('#reg_password').val()
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data, textStatus, jqxhr){
+      //set token and store
+      // automatically log user in when they register
+      $('#logout').show();  // show logout button
+      $('#login-register').hide();
+    }).fail(function(jqshr, textStatus, errorThrown){
+      alert('Login failed. Please use correct email and password.');
+    });
+  });
 
+  // FIX ME -- need to match credentials keys to backend db
+  // user login
+  $('#login').on('click', function() {
+    $.ajax(sa + '/login', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $('#lg_email').val(),
+          password: $('#lg_password').val()
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data, textStatus, jqxhr){
+      // set token and store
+      // automatically log user in when they register
+      $('#logout').show();  // show logout button
+      $('#login-register').hide();
+    }).fail(function(jqshr, textStatus, errorThrown){
+      console.log('login failed');
+      alert('Login failed. Please make sure your email and password are correct.');
+    });
+  });
 
 
 });
