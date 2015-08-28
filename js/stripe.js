04afcb04163719d449aa4b11d80b0c50434216c5
Bernard Lee <bernardlee82@gmail.com>
2015-08-27 22:27:48 -0400

    Add Stripe JavaScript module.
    Add basic Stripe module methods to create a charge object and send it to
    the back end.

---
+++ js/stripe.js
@@ -0,0 +1,66 @@
+'use strict';
+
+define(function() {
+	var orderInfo;
+	var stripe = {};
+
+	// charge object should look like this. these are just placeholder
+	// default values.
+	var charge = {
+		amount: 0,
+		currency: 'usd',
+		source:  {
+			number: 4242424242424242,
+			cvc: 123,
+			exp_month: 3,
+			exp_year: 2018
+		},
+		description: 'Purchase description.'
+	};
+
+	stripe.createCharge = function() {
+		console.log('create charge clicked');
+		charge.currency = 'usd';
+		charge.description = 'Syntactic Sugar Purchase';
+		charge.source.number = $('#cc-number').val();
+		charge.source.exp_month =$('#exp-month').val();
+		charge.source.exp_year = $('#exp-year').val();
+		charge.source.cvc = $('#cvc').val();
+
+		$.ajax(sa + '/cart/' + simpleStorage.get('user_info').user_id, {
+			contentType: 'application/json',
+			processData: false,
+			dataType: 'json',
+			method: 'GET'
+		})
+		.done(function(response) {
+			orderInfo = response;
+			charge.amount = 100 * orderInfo.totals.grandTotal;
+			console.log(charge);
+			stripe.sendCharge(charge);
+		})
+		.fail(function(response) {
+			console.error('stripe.createCharge() error');
+			console.error(response);
+		});
+	};
+
+	stripe.sendCharge = function(charge) {
+		$.ajax(sa + '/checkout', {
+			contentType: 'application/json',
+			processData: false,
+			dataType: 'json',
+			method: 'POST',
+			data: charge
+		})
+		.done(function(response) {
+			// if response is successful then add cart contents to past orders
+		})
+		.fail(function(response) {
+			console.error('stripe.sendCharge() error');
+			console.error(response);
+		});
+	};
+
+	return stripe;
+});
